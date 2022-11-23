73e30702a32b7a8d8cce9793ea653ae5c6dc5e4e
yeahg-dev <yeahg.dev@gmail.com>
2022-11-23 09:49:36 +0900

    project: iOS Project 생성 및 폴더링

---
+++ filter-video/filter-video/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  filter-video
+//
+//  Created by Moon Yeji on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
