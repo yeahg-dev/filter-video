8585d8774cee1ddd058383e3a0b692229c32589e
yeahg-dev <yeahg.dev@gmail.com>
2022-11-23 16:46:23 +0900

    FEAT: 피커로 이미지 선택 기능 구현

--- filter-video/filter-video/Controller/MenuViewController.swift
+++ filter-video/filter-video/Controller/MenuViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Moon Yeji on 2022/11/23.
 //
 
+import AVKit
+import MobileCoreServices
 import UIKit
 
 class MenuViewController: UIViewController {
@@ -14,6 +16,33 @@ class MenuViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func selectVideoDidTapped(_ sender: UIButton) {
+        VideoHelper.startMediaBrowser(
+            delegate: self,
+            sourceType: .savedPhotosAlbum)
+    }
+    
 }
 
+extension MenuViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL
+        else { return }
+        
+        dismiss(animated: true) {
+            let player = AVPlayer(url: url)
+            let playerVC = AVPlayerViewController()
+            playerVC.player = player
+            self.present(playerVC, animated: true, completion: nil)
+        }
+    }
+}
+
+extension MenuViewController: UINavigationControllerDelegate {
+    
+}
---
+++ filter-video/filter-video/Utility/VideoHelper.swift
@@ -0,0 +1,29 @@
+//
+//  VideoHelper.swift
+//  filter-video
+//
+//  Created by Moon Yeji on 2022/11/23.
+//
+
+import UIKit
+import MobileCoreServices
+
+struct VideoHelper {
+    
+    static func startMediaBrowser(
+      delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate,
+      sourceType: UIImagePickerController.SourceType
+    ) {
+      guard UIImagePickerController.isSourceTypeAvailable(sourceType) else {
+          return
+      }
+
+      let mediaUI = UIImagePickerController()
+      mediaUI.sourceType = sourceType
+      mediaUI.mediaTypes = [kUTTypeMovie as String]
+      mediaUI.allowsEditing = true
+      mediaUI.delegate = delegate
+      delegate.present(mediaUI, animated: true, completion: nil)
+    }
+    
+}
